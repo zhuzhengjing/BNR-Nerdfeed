47e61d08b84c0a4b7237f053f0070c0f2eba4030
朱正晶 <zhuzhengjing@zhuzhengjingdeMac.local>
2015-03-25 21:36:16 +0800

    完成第21章中级练习：加强UIWebView

--- Nerdfeed/BNRWebViewController.m
+++ Nerdfeed/BNRWebViewController.m
@@ -8,14 +8,69 @@
 
 #import "BNRWebViewController.h"
 
+@interface BNRWebViewController() <UIWebViewDelegate>
+@property (nonatomic, strong) UIToolbar *histroyToolBar;
+@property (nonatomic, strong) UIWebView *webView;
+@property (nonatomic, strong) UIBarButtonItem *goBackBarItem;
+@property (nonatomic, strong) UIBarButtonItem *goHeadBarItem;
+@end
+
+
 @implementation BNRWebViewController
 
 - (void)loadView
 {
-    UIWebView *webView = [[UIWebView alloc] init];
-    webView.scalesPageToFit = YES;
-    self.view = webView;
+    CGRect rect = [UIScreen mainScreen].bounds;
+    
+    self.webView = [[UIWebView alloc] init];
+    self.webView.scalesPageToFit = YES;
+    self.view = self.webView;
+    _histroyToolBar = [[UIToolbar alloc] initWithFrame:CGRectMake(0, rect.size.height - 50, rect.size.width, 50)];
+    _histroyToolBar.tintColor = [UIColor blackColor];
+    [self.view addSubview:self.histroyToolBar];
+    self.goBackBarItem = [[UIBarButtonItem alloc]
+                          initWithTitle:@"Back"
+                          style:UIBarButtonItemStylePlain
+                          target:self action:@selector(webViewGoBack)];
+    self.goHeadBarItem = [[UIBarButtonItem alloc]
+                          initWithTitle:@"Head"
+                          style:UIBarButtonItemStylePlain
+                          target:self action:@selector(webViewGoHead)];
+    UIBarButtonItem *flexibleSpace = [[UIBarButtonItem alloc]
+                                      initWithBarButtonSystemItem:UIBarButtonSystemItemFlexibleSpace
+                                      target:self
+                                      action:nil];
+    self.histroyToolBar.items = @[self.goBackBarItem, flexibleSpace, self.goHeadBarItem];
+    
+    [self updateBarItem];
 }
+
+- (void)webViewDidStartLoad:(UIWebView *)webView
+{
+    [self updateBarItem];
+}
+
+- (void)webViewDidFinishLoad:(UIWebView *)webView
+{
+    [self updateBarItem];
+}
+
+- (void)updateBarItem
+{
+    self.goBackBarItem.enabled = self.webView.canGoBack;
+    self.goHeadBarItem.enabled = self.webView.canGoForward;
+}
+
+- (void)webViewGoBack
+{
+    [self.webView goBack];
+}
+
+- (void)webViewGoHead
+{
+    [self.webView goForward];
+}
+
 
 - (void)setUrl:(NSURL *)url
 {
