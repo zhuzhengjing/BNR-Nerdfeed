2906c507357621cda640f48af60cc3c08162cd84
朱正晶 <zhuzhengjing@zhuzhengjingdeMac.local>
2015-03-25 08:01:33 +0800

    Initial Commit

---
+++ Nerdfeed/BNRAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  BNRAppDelegate.h
+//  Nerdfeed
+//
+//  Created by 朱正晶 on 15-3-25.
+//  Copyright (c) 2015年 China. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BNRAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Nerdfeed/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Nerdfeed
+//
+//  Created by 朱正晶 on 15-3-25.
+//  Copyright (c) 2015年 China. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "BNRAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([BNRAppDelegate class]));
+    }
+}
